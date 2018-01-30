aa09b8e0738648bc9806328c48e6da56488b15ec
abon-Floweraura <abon.duttaroy@bakingo.com>
2018-01-31 00:06:13 +0530

    master basic databse init

---
+++ database/basic_inventory_management01.sql
@@ -0,0 +1,33 @@
+CREATE TABLE p1_category(
+	cat_id varchar(12) PRIMARY KEY,
+    cat_name varchar(255),
+    created_at varchar(12),
+    updated_at varchar(12)
+);
+
+create TABLE p1_items(
+	item_id varchar(12) PRIMARY KEY,
+    cat_id varchar(12),
+    item_qunatity integer,
+    item_name varchar(255),
+    created_at varchar(12),
+    updated_at varchar(12)
+    
+);
+
+create TABLE p1_sku_details(
+	sku_id varchar(12) PRIMARY KEY,
+    sku_qunatity integer,
+    sku_name varchar(255),
+    is_open ENUM('0','1'),
+    created_at varchar(12),
+    updated_at varchar(12)
+    
+);
+
+CREATE table p1_sku_item(
+	sku_id varchar(12),
+	item_id varchar(12),
+    item_quantity integer,
+    PRIMARY key(sku_id,item_id)
+)
