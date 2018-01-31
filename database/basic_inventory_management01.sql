29520fd8e0182f2e2b210d9e032d30f520f9b6ab
abon-Floweraura <abon.duttaroy@bakingo.com>
2018-01-31 12:37:37 +0530

    master 31-1-18

--- database/basic_inventory_management01.sql
+++ database/basic_inventory_management01.sql
@@ -1,14 +1,15 @@
 CREATE TABLE p1_category(
-	cat_id varchar(12) PRIMARY KEY,
+    cat_id varchar(12) PRIMARY KEY,
     cat_name varchar(255),
     created_at varchar(12),
     updated_at varchar(12)
 );
 
 create TABLE p1_items(
-	item_id varchar(12) PRIMARY KEY,
+    item_id varchar(12) PRIMARY KEY,
     cat_id varchar(12),
     item_qunatity integer,
+    item_price number(12,2),
     item_name varchar(255),
     created_at varchar(12),
     updated_at varchar(12)
@@ -16,9 +17,11 @@ create TABLE p1_items(
 );
 
 create TABLE p1_sku_details(
-	sku_id varchar(12) PRIMARY KEY,
+    sku_id varchar(12) PRIMARY KEY,
     sku_qunatity integer,
     sku_name varchar(255),
+    sku_price number(12,2),
+    sku_manufacturing_price number(12,2),
     is_open ENUM('0','1'),
     created_at varchar(12),
     updated_at varchar(12)
@@ -26,8 +29,8 @@ create TABLE p1_sku_details(
 );
 
 CREATE table p1_sku_item(
-	sku_id varchar(12),
-	item_id varchar(12),
+    sku_id varchar(12),
+    item_id varchar(12),
     item_quantity integer,
     PRIMARY key(sku_id,item_id)
 )
